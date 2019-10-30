ad433f7c65d2820f6a7c453f360ab67953119723
gultiaeva <angultiaev@gmail.com>
2019-10-30 22:49:46 +0300

    modified __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,9 @@ config = {'extensions': ['.js', '.css', '.csv'], 'hash_size': 5}
 cache_buster = CacheBuster(config=config)
 cache_buster.init_app(app)
 data = pd.read_csv('./app/data/data.csv')
-
+data = data[['EmploymentField', 'EmploymentStatus', 
+             'Gender', 'LanguageAtHome', 
+             'JobWherePref', 'SchoolDegree', 
+             'Income']]
+data = data[data['Gender'].isin(['male', 'female'])]
+data.dropna(inplace=True)
