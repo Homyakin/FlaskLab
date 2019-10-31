29768bc53b33a1a084948da5d2b9e0e2e50f92f3
Homyakin <tox.home@ya.ru>
2019-10-31 18:09:03 +0300

    [RF] some improvements

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,14 @@ from flask import Flask
 import pandas as pd
 from flask_cachebuster import CacheBuster
 
-
 app = Flask(__name__)
 config = {'extensions': ['.js', '.css', '.csv'], 'hash_size': 5}
 cache_buster = CacheBuster(config=config)
 cache_buster.init_app(app)
 data = pd.read_csv('./app/data/data.csv')
-data = data[['EmploymentField', 'EmploymentStatus', 
-             'Gender', 'LanguageAtHome', 
-             'JobWherePref', 'SchoolDegree', 
+data = data[['EmploymentField', 'EmploymentStatus',
+             'Gender', 'LanguageAtHome',
+             'JobWherePref', 'SchoolDegree',
              'Income']]
 data = data[data['Gender'].isin(['male', 'female'])]
 data.dropna(inplace=True)
