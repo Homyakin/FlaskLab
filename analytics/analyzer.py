b15530143547a58f3d54f5b1dcee2a1054f4ce4f
gultiaeva <angultiaev@gmail.com>
2019-10-31 18:38:02 +0300

    minor fix

--- analytics/analyzer.py
+++ analytics/analyzer.py
@@ -8,23 +8,23 @@ from scipy.stats import chi2_contingency
 
 
 def get_contingency_table(field1: str, field2: str):
-    '''
+    """
 
     :param field1:
     :param field2:
     :return: таблица сопряженности
-    '''
+    """
 
     cross_table = pd.crosstab(data[field1], data[field2])
     return cross_table
 
 
 def pearsons_chi2(crosstab, correction=False):
-    '''
-    :param crosstab: numpy.ndarray -- table of actual frequences
+    """
+    :param crosstab: numpy.ndarray -- table of actual frequencies
     :param correction: bool -- use Yates correction
     :return: tuple -- (chi2 statistic, p-value, table of expected values)
-    '''
+    """
     chi2_stat, pval, df, expected = chi2_contingency(crosstab,
                                                      correction=correction)
 
@@ -38,11 +38,11 @@ def pearsons_chi2(crosstab, correction=False):
 
 
 def exact_fisher(crosstab, correction=False):
-    '''
-    :param crosstab: numpy.ndarray -- table of actual frequences
+    """
+    :param crosstab: numpy.ndarray -- table of actual frequencies
     :param correction: bool -- use Yates correction
     :return: tuple -- (chi2 statistic, None)
-    '''
+    """
     numpy2ri.activate()
     stats = importr('stats')
 
@@ -56,19 +56,20 @@ def exact_fisher(crosstab, correction=False):
 
 
 def get_statistic_and_expected_table(field1: str, field2: str, crosstab):
-    '''
+    """
         super duper AI function for choosing tests
 
         :param field1:
         :param field2:
         :param crosstab: pd.crosstab() for field1 and field2:
         :return: tuple (function result, table of expected values or None)
-    '''
-    if np.all(crosstab > 10):
-        return pearsons_chi2(crosstab, False)
-    elif np.any((crosstab >= 5) & (crosstab <= 9)):
-        return pearsons_chi2(crosstab, True)
-    elif np.sum(crosstab) > 500:
+    """
+    if np.sum(crosstab) > 500:
         return exact_fisher(crosstab, True)
     else:
-        return exact_fisher(crosstab, False)
+        if np.all(crosstab >= 10):
+            return pearsons_chi2(crosstab, False)
+        elif np.any((crosstab > 5) & (crosstab <= 9)):
+            return pearsons_chi2(crosstab, True)
+        else:
+            return exact_fisher(crosstab, False)
